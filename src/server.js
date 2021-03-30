c3135f6d84c13ca1f3c52b5c47d69f86beb32e49
Iago Guedelha Nunes <iagognunes@outlook.com>
2021-03-30 16:53:54 -0300

    Aula 01

--- src/server.js
+++ src/server.js
@@ -2,5 +2,11 @@ const express = require("express");
 const server = express();
 
 server.listen(3000, () => {
-    
+    console.log('Rodando o servidor na porta 3000');
 });
+
+server.get('/', (request, response) => {
+    return response.send('Teste!');
+});
+
+//Parei em 2h39m52
